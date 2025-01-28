1a6d5f082764fb34d91fcac2c007ea62d51af803
gabeFrancisco <gabegit2@gmail.com>
2025-01-27 21:47:00 -0300

    revisão parte 1

---
+++ src/main.py
@@ -0,0 +1,27 @@
+#Variáveis
+
+print("Hello world! God bless you!")
+age = 25
+name = 'Gabriel'
+hasChildren = True
+
+print(f"I'm {name}!")
+
+#Inputs
+book = input("Tell me a book: ")
+print(book)
+
+#Listas
+
+books = ['Bible', 'Harry Potter and the Philosopher Stone', 'Python for Dummies', 'Design Patterns']
+
+print(books[0])
+print(books[1].title())
+
+#Insere um livro na lista
+newBook = input("Insira um novo livro na lista:")
+books.append(newBook)
+print(books[-1])
+
+
+
