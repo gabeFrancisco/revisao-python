4a9e5d4c57896c06924fa58ef2b6979395b68ea0
gabeFrancisco <gabegit2@gmail.com>
2025-01-31 23:21:19 -0300

    separado modulos em funções

--- src/main.py
+++ src/main.py
@@ -1,27 +1,58 @@
-#Variáveis
+#Funções para definir em que módulo entrar:
 
-print("Hello world! God bless you!")
-age = 25
-name = 'Gabriel'
-hasChildren = True
+def variaveis():
+    #Variáveis
 
-print(f"I'm {name}!")
+    print("Hello world! God bless you!")
+    age = 25
+    name = 'Gabriel'
+    hasChildren = True
 
-#Inputs
-book = input("Tell me a book: ")
-print(book)
+    print(f"I'm {name}!")
 
-#Listas
+def inputs():
 
-books = ['Bible', 'Harry Potter and the Philosopher Stone', 'Python for Dummies', 'Design Patterns']
+    #Inputs
+    book = input("Tell me a book: ")
+    print(book)
 
-print(books[0])
-print(books[1].title())
+def listas():
+    #Listas
+    books = ['Bible', 'Harry Potter and the Philosopher Stone', 'Python for Dummies', 'Design Patterns']
 
-#Insere um livro na lista
-newBook = input("Insira um novo livro na lista:")
-books.append(newBook)
-print(books[-1])
+    print(books[0])
+    print(books[1].title())
 
+    #Insere um livro na lista
+    newBook = input("Insira um novo livro na lista:")
+    books.append(newBook)
+    print(books[-1])
 
+#Functions
 
+def someFunction():
+    print("Hellooo! This function is working!")
+
+someFunction()
+
+#For loops
+
+def forLoops():
+    for i in books:
+        print(i)
+
+#While loops
+
+def whileLoops():
+    counter = 0
+    
+opcao = int(input("Escolha uma opção: \n1-Variáveis\n2-Inputs\n3-Listas\n4-Loop For\n5-Loop While"))
+
+if(opcao == 1):
+    variaveis()
+elif(opcao == 2):
+    inputs()
+elif(opcao == 3):
+    listas()
+
+   
